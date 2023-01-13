f2be2829c8e9f5175552ec02ff4887465eed00dc
nnnn777 <ngallo1@protonmail.com>
2023-01-12 23:36:01 -0500

    Minimal example + GUI pages
    Minimal example will be posted to streamlit discussion forum.
    
    GUI: more control to play around

--- aggrid_select.py
+++ aggrid_select.py
@@ -4,23 +4,25 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode
 from st_aggrid.shared import ColumnsAutoSizeMode
 
 
-# ** todo: this is a common way to display tables across the application ....
 def st_aggrid_dataframe(
         df,
         selection_mode="disabled",
-        pre_select_all_rows=True,
+        pre_select_all_rows=False,
     ):
     """
-    Display a pd.DataFrame and allow user to select a single or multiple rows
-    Usage taken from demo at:https://pablocfonseca-streamlit-aggrid-examples-example-jyosi3.streamlitapp.com/
+    Display a pd.DataFrame and allow user to select a single row or multiple rows
+    Inspired by demo at: https://pablocfonseca-streamlit-aggrid-examples-example-jyosi3.streamlitapp.com/
     """
     if selection_mode not in ["disabled", "single", "multiple"]:
         raise Exception(f"Invalid selection_mode: {selection_mode}")
+    
+    # Process the DataFrame for aggrid display
     original_index = df.index
     df = (df.to_frame() if isinstance(df, pd.Series) else df)
     df = df.reset_index()   # aggrid doesn't show DataFrame index
     df.columns = [str(c) for c in df.columns]    # bug: GridOptionsBuilder only allows string column names
 
+    # Build aggrid options
     gb = GridOptionsBuilder.from_dataframe(df)
     if selection_mode=="multiple":
         gb.configure_selection(
@@ -33,7 +35,7 @@ def st_aggrid_dataframe(
     gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=10)
     gridOptions = gb.build()
 
-    # Displays the DataFrame, event for select
+    # Display a DataFrame responsive to clicks
     grid_response = AgGrid(
         df,
         gridOptions=gridOptions,
@@ -41,6 +43,7 @@ def st_aggrid_dataframe(
         columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS,
     )
     
+    # Parse selected DataFrame rows from aggrid response object
     if selection_mode=="disabled":
         return None
     else:
@@ -49,7 +52,8 @@ def st_aggrid_dataframe(
             None if not selected else
             [int(sel["_selectedRowNodeInfo"]["nodeId"]) for sel in selected]
         )
-        return (
+        selected_rows = (
             None if selected_row_idxs is None else
             original_index[selected_row_idxs]
         )
+        return selected_rows
