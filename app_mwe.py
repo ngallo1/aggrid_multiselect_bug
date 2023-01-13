646809fa665dfff0c44502953f25adbc67d22bd9
nnnn777 <ngallo1@protonmail.com>
2023-01-12 19:41:10 -0500

    Update app_mwe.py

--- app_mwe.py
+++ app_mwe.py
@@ -9,7 +9,7 @@ from st_aggrid.shared import ColumnsAutoSizeMode
 
 
 # Make sample data
-number_of_rows = 100 
+number_of_rows = 100
 number_of_columns = 5
 df = pd.DataFrame(
     data=np.arange(number_of_rows*number_of_columns).reshape((number_of_rows, number_of_columns))
@@ -21,8 +21,8 @@ df.columns = [str(c) for c in df.columns]    # bug: GridOptionsBuilder only allo
 
 # Displays the DataFrame and get click events
 # Count the number of warnings it throws.
-warning_count = 0
 with warnings.catch_warnings(record=True) as warns:
+    st.header("Aggrid Data Selection")
     # Set grid options.
     # Bug seems to occur when selection_mode="multiple" and all rows are pre-pre-selected
     gb = GridOptionsBuilder.from_dataframe(df)
@@ -43,9 +43,21 @@ with warnings.catch_warnings(record=True) as warns:
     )
 
     # Warning ....
+    st.subheader("Warning Information")
     if "warning_count" not in st.session_state:
         st.session_state["warning_count"] = 0
     st.session_state["warning_count"] += len(warns)
-    warning_count += len(warns)
     st.write(f"""Number of warnings since app start: {st.session_state["warning_count"]}""")
-    st.write(f"warning_count:  {warning_count}")
+
+    st.write("Most recent warnings:")
+    st.write(warns)
+    for w in warns:  st.write(w)
+
+st.subheader("Warning Discussion")
+st.markdown("""
+- The aggrid dataframe is slow to load and throws many warnings, each resulting from a call to st_aggrid.\_\_init\_\_.py.
+
+- This occurs when using selection_mode="multiple", with all rows initially selected, and with multiple pages of data.  The problem is more severe for larger datasets (not shown).
+
+- I encounter this problem only on Streamlit Cloud and not my local machine.
+""")
